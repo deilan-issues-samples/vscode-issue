99853a7388489399e343b0e32aeb6f86aeed6dac
Deilan <deilan@users.noreply.github.com>
2018-06-24 20:04:48 +0300

    add xhr sample

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
-const source = Observable.from([1,2,3,4,5]);
-source.subscribe(
-  (data) => document.writeln(`${data}<br>`)
+function loadUsers() {
+  return Observable.create((o: Observer<void>) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      if(xhr.status !== 200) {
+        o.error(xhr.statusText);
+        return;
+      }
+      try {
+        const data = JSON.parse(xhr.responseText);
+        o.next(data);
+        o.complete();
+      } catch (err) {
+        o.error(err);
+      }
+    });
+    xhr.open('GET', 'https://api.github.com/users3');
+    xhr.send();
+  }).retryWhen(retryStrategy());
+}
+
+function retryStrategy() {
+  return function(err: any) {
+    return err.delay(1000);
+  }
+}
+
+loadUsers().subscribe(
+  (data: any) => console.log(`next: ${data}`),
+  (err: any) => console.log(`error: ${err}`),
+  () => console.log('complete')
 );
